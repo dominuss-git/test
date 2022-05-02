ba3dadc76d69707e92d3bfbc86bc45b0ecf08ac6
dominuss-git <kiril6701@gmail.com>
2022-05-02 23:54:33 +0300

    dfgh

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
-// const DISCOVERY_DOC = [
-//   "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-// ];
+const DISCOVERY_DOC = [
+  "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+];
 
 // // Authorization scopes required by the API; multiple scopes can be
 // // included, separated by spaces.
