6e69c0aebfb1eb9c738e8134062a329d1601019e
dominuss-git <kiril6701@gmail.com>
2022-05-15 15:33:08 +0300

    uygfcvbnmkj

--- js/login.js
+++ js/login.js
@@ -1,34 +1,23 @@
-// TODO(developer): Set to client ID and API key from the Developer Console
 const CLIENT_ID =
   "822723435779-entgq7gcoh7lsobv2pjlg1ihbevfsio6.apps.googleusercontent.com";
 const API_KEY = "AIzaSyBGqMCLntJ40IGU9HU1objWRgDqQw1GXdw";
 
-// Discovery doc URL for APIs used by the quickstart
 const DISCOVERY_DOC =
   "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
 
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
 const SCOPES = "https://www.googleapis.com/auth/calendar";
 
 let tokenClient;
 let gapiInited = false;
 let gisInited = false;
 
-document.getElementById("authorize_button").style.visibility = "hidden";
-document.getElementById("signout_button").style.visibility = "hidden";
+document.getElementById("authorize_button").style.display = "none";
+document.getElementById("signout_button").style.display = "none";
 
-/**
- * Callback after api.js is loaded.
- */
 function gapiLoaded() {
   gapi.load("client", intializeGapiClient);
 }
 
-/**
- * Callback after the API client is loaded. Loads the
- * discovery doc to initialize the API.
- */
 async function intializeGapiClient() {
   await gapi.client.init({
     apiKey: API_KEY,
@@ -38,119 +27,55 @@ async function intializeGapiClient() {
   maybeEnableButtons();
 }
 
-/**
- * Callback after Google Identity Services are loaded.
- */
 function gisLoaded() {
   tokenClient = google.accounts.oauth2.initTokenClient({
     client_id: CLIENT_ID,
     scope: SCOPES,
-    callback: "", // defined later
+    callback: "",
   });
   gisInited = true;
   maybeEnableButtons();
 }
 
-/**
- * Enables user interaction after all libraries are loaded.
- */
 function maybeEnableButtons() {
   if (gapiInited && gisInited) {
-    document.getElementById("authorize_button").style.visibility = "visible";
+    document.getElementById("authorize_button").style.display = "static";
   }
 }
 
-/**
- *  Sign in the user upon button click.
- */
 function handleAuthClick() {
   tokenClient.callback = async (resp) => {
     if (resp.error !== undefined) {
       throw resp;
     }
-    document.getElementById("signout_button").style.visibility = "visible";
+    document.getElementById("signout_button").style.display = "static";
     document.getElementsByClassName("main")[0].style.display = "grid";
-    // document.getElementsByClassName('main-wrapper')[0].style = "display: grid;";
-    // document.getElementsByClassName('general-task-wrapper')[0].style = "display: flex;";
     document.getElementsByClassName("wrapper-logout")[0].style =
       "display: none;";
 
-    document.getElementById("authorize_button").style.visibility = "hidden";
-    // await listUpcomingEvents();
+    document.getElementById("authorize_button").style.display = "none";
   };
 
   if (gapi.client.getToken() === null) {
-    // Prompt the user to select a Google Account and ask for consent to share their data
-    // when establishing a new session.
     tokenClient.requestAccessToken({ prompt: "consent" });
   } else {
-    // Skip display of account chooser and consent dialog for an existing session.
     tokenClient.requestAccessToken({ prompt: "" });
   }
 }
 
-/**
- *  Sign out the user upon button click.
- */
 function handleSignoutClick() {
   const token = gapi.client.getToken();
   if (token !== null) {
     google.accounts.oauth2.revoke(token.access_token);
     gapi.client.setToken("");
-    document.getElementById("content").innerText = "";
-    document.getElementById("authorize_button").style.visibility = "visible";
-    document.getElementById("signout_button").style.visibility = "hidden";
+    document.getElementById("authorize_button").style.display = "static";
+    document.getElementById("signout_button").style.display = "none";
     document.getElementsByClassName("main")[0].style.display = "none";
-    // document.getElementsByClassName('main')[0].classList = ['main-index'];
-    // document.getElementsByClassName('main-wrapper')[0].style = "display: none;";
-    // document.getElementsByClassName('general-task-wrapper')[0].style = "display: none;";
     document.getElementsByClassName("wrapper-logout")[0].style =
       "display: static;";
   }
 }
 
-/**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
-async function listUpcomingEvents() {
-  let response;
-  try {
-    const request = {
-      calendarId: "primary",
-      timeMin: new Date().toISOString(),
-      showDeleted: false,
-      singleEvents: true,
-      maxResults: 10,
-      orderBy: "startTime",
-      q: "BountyDo",
-    };
-    response = await gapi.client.calendar.events.list(request);
-  } catch (err) {
-    // document.getElementById("content").innerText = err.message;
-    return;
-  }
-
-  const events = response.result.items;
-  if (!events || events.length == 0) {
-    // document.getElementById("content").innerText = "No events found.";
-    return;
-  }
-  // Flatten to string to display
-  // tasks.default = events.map((task) => {
-  // return {
-  // nameOfTask: task.summary,
-
-  // }
-  // })
-  // const output = events.reduce(
-  // (str, event) =>
-  // `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
-  // "Events:\n"
-  // );
-  // document.getElementById("content").innerText = output;
-}
 function createCalendarMark(summary, date1, date2, description) {
   var event = {
     summary,
@@ -175,14 +100,8 @@ function createCalendarMark(summary, date1, date2, description) {
     },
   };
 
-  // console.log(event);
-
-  var request = gapi.client.calendar.events.insert({
+  gapi.client.calendar.events.insert({
     calendarId: "primary",
     resource: event,
-  });
-
-  request.execute(function (event) {
-    // appendPre("Event created: " + event.htmlLink);
   });
 }
